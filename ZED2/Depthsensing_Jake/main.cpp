2d32b06a11218fca22d7e42caf066213eaeba73f
gerhardmalberg88 <gerhardmalberg.gm@gmail.com>
2021-03-23 20:05:04 +0200

    Modified depth sensing testing, not finished

--- ZED2/Depthsensing_Jake/main.cpp
+++ ZED2/Depthsensing_Jake/main.cpp
@@ -18,6 +18,9 @@
 //
 ///////////////////////////////////////////////////////////////////////////
 
+// Depth sensing example by Jake using ZED V2 camera and ZED OSDK
+// Parts of samples from ZED OSDK are utilized
+
 
 #include <sl/Camera.hpp>
 
@@ -29,10 +32,16 @@ int main(int argc, char **argv) {
     // Create a ZED camera object
     Camera zed;
 
+    // Declare some stuff
+    string unit = "M";
+
     // Set configuration parameters
+    // Can be found in API documentation
     InitParameters init_parameters;
-    init_parameters.depth_mode = DEPTH_MODE::PERFORMANCE; // Use PERFORMANCE depth mode
-    init_parameters.coordinate_units = UNIT::MILLIMETER; // Use millimeter units (for depth measurements)
+    init_parameters.depth_mode = DEPTH_MODE::ULTRA; // Depth mode, available is ULTRA, QUALITY, PERFORMANCE
+    init_parameters.coordinate_units = UNIT::METER; // Unit to use (for depth measurements)
+    init_parameters.depth_minimum_distance = 0.3 ; // Minimum depth perception, minimum setting for ZED 2 is 0.3m. Increase to increase performance
+    // zed.setDepthMaxRangeValue(40); // set maximum depth perception distance to 40m
 
     // Open the camera
     auto returned_state = zed.open(init_parameters);
@@ -47,9 +56,21 @@ int main(int argc, char **argv) {
 
     // Capture 50 images and depth, then stop
     int i = 0;
+    int j = 0;
+    int k = 0;
     sl::Mat image, depth, point_cloud;
 
-    while (i < 50) {
+
+    // Define a matrix of sectors
+    int mat_columns = 128;
+    int mat_rows = 72;
+
+    int mat_xoffset, mat_yoffset;
+
+    int x = 0;
+    int y = 0;
+
+    while (i < 5) {
         // A new image is available if grab() returns ERROR_CODE::SUCCESS
         if (zed.grab(runtime_parameters) == ERROR_CODE::SUCCESS) {
             // Retrieve left image
@@ -59,18 +80,37 @@ int main(int argc, char **argv) {
             // Retrieve colored point cloud. Point cloud is aligned on the left image.
             zed.retrieveMeasure(point_cloud, MEASURE::XYZRGBA);
 
-            // Get and print distance value in mm at the center of the image
+            // Get and print distance value
             // We measure the distance camera - object using Euclidean distance
-            int x = image.getWidth() / 2;
-            int y = image.getHeight() / 2;
+            // Note: x and y coordinates are in pixels, with 0,0 at top left corner
+
+            // Decide offsets for each sector
+            mat_xoffset = image.getWidth() / mat_columns;
+            mat_yoffset = image.getHeight() / mat_rows;
+
+            // set start sector center
+            x = mat_xoffset / 2;
+            y = mat_yoffset / 2;
+
+            // Go through the columns
+            while (j <= mat_columns)
+
+            // This is nowhere near finished, do not use
+
+            // Probe the center of current sector
+            x = 0;
+            y = 0;
             sl::float4 point_cloud_value;
             point_cloud.getValue(x, y, &point_cloud_value);
 
             if(std::isfinite(point_cloud_value.z)){
-                float distance = sqrt(point_cloud_value.x * point_cloud_value.x + point_cloud_value.y * point_cloud_value.y + point_cloud_value.z * point_cloud_value.z);
-                cout<<"Distance to Camera at position {"<<x<<";"<<y<<"}: "<<distance<<"mm"<<endl;
-            }else
+                distance1 = sqrt(point_cloud_value.x * point_cloud_value.x + point_cloud_value.y * point_cloud_value.y + point_cloud_value.z * point_cloud_value.z);
+                // cout<<"Distance to Camera at position {"<<x<<";"<<y<<"}: "<<distance<<"mm"<<endl;
+            }else {
                 cout<<"The Distance can not be computed at {"<<x<<";"<<y<<"}"<<endl;
+                distance1 = 0;
+                }
+
 
             // Increment the loop
             i++;
@@ -81,8 +121,8 @@ int main(int argc, char **argv) {
     cout<<"Image width: "<<image.getWidth()<<endl;
     cout<<"Image height: "<<image.getHeight()<<endl;
 
-    // Test print statement
-    cout<<"Version three"<<endl;
+    // End message
+    cout<<"Exiting program"<<endl;
     // Close the camera
     zed.close();
     return EXIT_SUCCESS;
---
+++ ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/ZED_Object_detection_image_viewer.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/3.10.2/CompilerIdC/CMakeCCompilerId.c" "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/ZED_Object_detection_image_viewer.dir/CMakeFiles/3.10.2/CompilerIdC/CMakeCCompilerId.o"
+  "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/feature_tests.c" "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/ZED_Object_detection_image_viewer.dir/CMakeFiles/feature_tests.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/zed/include"
+  "/usr/local/cuda/include"
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/3.10.2/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/ZED_Object_detection_image_viewer.dir/CMakeFiles/3.10.2/CompilerIdCXX/CMakeCXXCompilerId.o"
+  "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/GLViewer.cpp" "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/ZED_Object_detection_image_viewer.dir/GLViewer.o"
+  "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/main.cpp" "/home/uwb5/git/ZED2/zed-examples/object detection/image viewer/cpp/src/CMakeFiles/ZED_Object_detection_image_viewer.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/zed/include"
+  "/usr/local/cuda/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
