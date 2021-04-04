f2f16366f995183646a096cfb8d5b8f51b606f01
gerhardmalberg88 <gerhardmalberg.gm@gmail.com>
2021-04-05 02:01:31 +0300

    Added git using visual studio code;

--- TESTS/cpp_tests/MyLibrary.hpp
+++ TESTS/cpp_tests/MyLibrary.hpp
@@ -16,7 +16,7 @@
 #ifndef MYLIBRARY_H                                 // Guards
 #define MYLIBRARY_H                                 // Guards
 
-// Definitions (These definitions are different than function deffinitions)
+// Definitions (These definitions are different than function definitions)
 #define FILE_SIZE 42                                // This defines that FILE_SIZE is same as 42 THIS IS NOT VARIABLE DEFINITION
 #define MESSAGE "Hello"                             // This defines that MESSAGE is same as "Hello" THIS IS NOT VARIABLE DEFINITION
 
@@ -25,7 +25,7 @@
 #include <cstdlib>                                  // Header in standard library
 #include <unistd.h>                                 // Header in standard library
 // It is good practice to include all libraries in header file that are used in your library source file and only include this header 
-// library file in your library soure file
+// library file in your library source file
 
 namespace MyNameSpaceFirst                          // Namespace declaration
 {
@@ -36,32 +36,32 @@ namespace MyNameSpaceFirst                          // Namespace declaration
             private:
                 int OnlyPrivateIntegerOne;          // Private Attribute(Variable) declaration THIS IS CAN NOT BE ACCESSED OUTSIDE OF CURRENT CLASS
             public:
+                ~MyClassOne();                      // Destructor declaration
                 void PrintFromFirstClass(void);     // Function declaration 
-                //void ~PrintFromFirstClass(void);  // Destructor is not needed this time but is here to show
                 int PublicIntegerOne;               // Attribute(Variable) declaration
                 typedef struct MyStructOne
                 {
                     int MyIntInStructOne;                                   // Declaration of member in struct THIS CAN BE READ AND WRITE BY SOURCE FILE
-                    int MyIntInStructTwo = 2222;                            // Declaration of member in struct THIS CAN BE READ AND WRITE BY SOURCE FILE
+                    int MyIntInStructTwo = 2;                               // Declaration of member in struct THIS CAN BE READ AND WRITE BY SOURCE FILE
                     const static int MyIntInStructThree = 3;                // Declaration of member in struct THIS CAN ONLY BE READ AND NEED TO BE DEFINED IN HEADER ONLY
                     char MyIntInStructFour = 'C';                           // Declaration of member in struct THIS CAN BE READ AND WRITE BY SOURCE FILE
-                    float MyIntInStructFive = 3.1456785;                    // Declaration of member in struct THIS CAN BE READ AND WRITE BY SOURCE FILE
-                    char MyIntInStructSix[10] = "MMMMOO";                   // Declaration of member in struct THIS CAN ONLY BE READ AND NEED TO BE DEFINED IN HEADER ONLY
+                    float MyIntInStructFive = 3.1414141;                    // Declaration of member in struct THIS CAN BE READ AND WRITE BY SOURCE FILE
+                    char MyIntInStructSix[10] = "DRONE";                    // Declaration of member in struct THIS CAN ONLY BE READ AND NEED TO BE DEFINED IN HEADER ONLY
 
                 }MyStructOne;
-                enum MyEnumOne                      // Enum declaration
+                typedef enum MyEnumOne              // Enum declaration
                 {
-                    MyEnumMemberOne = 1,            // Enum member declaration
-                    MyEnumMemberTwo = 2             // Enum member declaration
-                };
+                    MyEnumMemberOne = 1,       // Enum member declaration
+                    MyEnumMemberTwo = 2        // Enum member declaration
+                }MyEnumOne;
         };
         class MyClassTwo                            // Class declaration
         {
             private:
                 float OnlyPrivateFloatTwo;          // Private Attribute(Variable) declaration THIS IS CAN NOT BE ACCESSED OUTSIDE OF CURRENT CLASS
             public:
+                ~MyClassTwo();                      // Destructor declaration
                 void PrintFromSecondClass(void);    // Method(Function) declaration
-                //void ~PrintFromSecondClass(void); // Destructor is not needed this time but is here to show
                 int PublicIntegerTwo;               // Attribute(Variable) declaration
         };
     }
--- TESTS/cpp_tests/main.cpp
+++ TESTS/cpp_tests/main.cpp
@@ -1,31 +1,38 @@
+/* 
+TODO
++ Enum access
++ Struct pointer passing to function
++ Makefile
+
+
+*/
 // Example of main source file
 // Library Source file must have
 // 1. Includes
 // 2. Namespace use or not
-// 3. Instances(objects) of class
+// 3. Instances(objects) of classes
 // 4. Calling object methods(functions) and writing or reading attributtes(variables)
 
 // Includes
 #include "MyLibrary.hpp"
 
-using namespace MyNameSpaceFirst;                                                                   // Namespace definition
-using namespace MyNameSpaceFirst::MyNameSpaceSecond;                                                // Namespace definition
+using namespace MyNameSpaceFirst;                                                               // Namespace definition (OPTIONAL)
+using namespace MyNameSpaceFirst::MyNameSpaceSecond;                                            // Namespace definition (OPTIONAL)
+MyClassOne::MyStructOne InstanceStructOne;                                                      // Create instance of struct (utilizing namespace)
 
-MyClassOne::MyStructOne InstanceStructOne;
-MyClassOne::MyEnumOne InstanceEnumOne; 
+int main()                                                                                      // Main function definition without arguments ARGUMENTS ARE OPTIONAL
+{                                                                                               // THIS MEANS when running program ./main argument1 argument2
+    MyClassOne MyInstanceClassOne;                                                              // Create instance of class from the library
+    MyInstanceClassOne.PrintFromFirstClass();                                                   // Call function of class form the library
+    InstanceStructOne.MyIntInStructOne = 88;                                                    // Overwrite value for member of struct in class
+    std::cout << "MyIntInStructOne = " <<InstanceStructOne.MyIntInStructOne<< std::endl;        // Access to struct
+    std::cout << "MyIntInStructTwo = " <<InstanceStructOne.MyIntInStructTwo<< std::endl;        // Access to struct
+    std::cout << "MyIntInStructThree = " <<InstanceStructOne.MyIntInStructThree<< std::endl;    // Access to struct
+    std::cout << "MyIntInStructFour = " <<InstanceStructOne.MyIntInStructFour<< std::endl;      // Access to struct
+    std::cout << "MyIntInStructFive = " <<InstanceStructOne.MyIntInStructFive<< std::endl;      // Access to struct
+    std::cout << "MyIntInStructSix = " <<InstanceStructOne.MyIntInStructSix<< std::endl;        // Access to struct
 
-int main()                                                                                          // Main function with arguments input option 
-{                                                                                                   // THIS MEANS when running program ./main argument1 argument2
-    MyClassOne MyInstanceClassOne;                                                                  // Create instance of class from the library
-    MyInstanceClassOne.PrintFromFirstClass();                                                       // Call function of class form the library
-    InstanceStructOne.MyIntInStructOne = 88;
-    std::cout << "MyIntInStructOne = " <<InstanceStructOne.MyIntInStructOne<< std::endl;
-    std::cout << "MyIntInStructTwo = " <<InstanceStructOne.MyIntInStructTwo<< std::endl;
-    std::cout << "MyIntInStructThree = " <<InstanceStructOne.MyIntInStructThree<< std::endl;
-    std::cout << "MyIntInStructFour = " <<InstanceStructOne.MyIntInStructFour<< std::endl;
-    std::cout << "MyIntInStructFive = " <<InstanceStructOne.MyIntInStructFive<< std::endl;
-    std::cout << "MyIntInStructSix = " <<InstanceStructOne.MyIntInStructSix<< std::endl;
+    std::cout << "MyEnum = " <<MyClassOne::MyEnumMemberOne<< std::endl;                         // Access to enum (CAN BE DONE DIRECTLY
 
-    std::cout << "MyIntInStructSix = " <<InstanceEnumOne.MyEnumMemberOne<< std::endl;
     return 0;
 }
