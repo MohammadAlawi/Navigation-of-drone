ccebc24e85dd2732889fd3bd9b7f9e059a19154c
uwb4 <anton.eklund@aalto.fi>
2021-04-08 00:48:48 +0300

    Python to cpp files

--- POZYX/Python-to-cpp/Python_to_cpp_test.cpp
+++ POZYX/Python-to-cpp/Python_to_cpp_test.cpp
@@ -1,18 +1,67 @@
+/*
 #include <stdio.h>
-#include <conio.h>
-#include <Python.h>
+#include "python3.6/Python.h"
 
 int main()
 {
-	PyObject* pInt;
+	char filename[] = "testfile.py";
+	FILE* fp;
 
 	Py_Initialize();
 
+	fp = _Py_fopen(filename, "r");
+	PyRun_SimpleFile(fp, filename);
+
+	Py_FinalizeEx();
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+#include "./include/pyhelper.hpp"
+
+int main()
+{
+    CPyInstance pyInstance;
+
 	PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
 	
-	Py_Finalize();
 
 	printf("\nPress any key to exit...\n");
-	if(!_getch()) _getch();
+	return 0;
+}*/
+
+#include <stdio.h>
+#include "python3.6/Python.h"
+#include "./include/pyhelper.hpp"
+
+int main()
+{
+	CPyInstance hInstance;
+
+	CPyObject pName = PyUnicode_FromString("testfile");
+	CPyObject pModule = PyImport_Import(pName);
+
+	if(pModule)
+	{
+		CPyObject pFunc = PyObject_GetAttrString(pModule, "getInteger");
+		if(pFunc && PyCallable_Check(pFunc))
+		{
+			CPyObject pValue = PyObject_CallObject(pFunc, NULL);
+
+			printf("C: getInteger() = %ld\n", PyLong_AsLong(pValue));
+		}
+		else
+		{
+			printf("ERROR: function getInteger()\n");
+		}
+
+	}
+	else
+	{
+		printf("ERROR: Module not imported\n");
+	}
+
 	return 0;
 }
---
+++ POZYX/Python-to-cpp/testfile.py
@@ -0,0 +1,4 @@
+def getInteger():
+    print('Python function getInteger() called')
+    c = 100*50/30
+    return c
